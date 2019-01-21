0472d261afb2a439fc4218f56c244f3f5b092c39
SarobidyToky <hastma12@gmail.com>
2019-01-21 23:27:24 +0300

    Ajout des deux entites

---
+++ src/Entity/Entree.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\EntreeRepository")
+ */
+class Entree
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $quantiteEntree;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $datePeremption;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getQuantiteEntree(): ?int
+    {
+        return $this->quantiteEntree;
+    }
+
+    public function setQuantiteEntree(int $quantiteEntree): self
+    {
+        $this->quantiteEntree = $quantiteEntree;
+
+        return $this;
+    }
+
+    public function getDatePeremption(): ?\DateTimeInterface
+    {
+        return $this->datePeremption;
+    }
+
+    public function setDatePeremption(\DateTimeInterface $datePeremption): self
+    {
+        $this->datePeremption = $datePeremption;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
